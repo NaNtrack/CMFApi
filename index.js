3dcec31c32794dd8834800a04169d59cd04898ca
Julio Araya Cerda <julioarayacerda@gmail.com>
2023-07-26 12:12:46 -0400

    Se agrega react-native-navigation y react-native-config

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {NavigationContainer} from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+function CMFApp() {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => CMFApp);
