b81ffd83761e867c3493e2703bd383b95df331b6
Julio Araya Cerda <julioarayacerda@gmail.com>
2023-07-26 20:34:53 -0400

    Fixed tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,14 +4,23 @@
 
 import 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import App from '../App';
 
 // Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
+import {it, expect} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer, {act} from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders correctly', async () => {
+  renderer.create(
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>,
+  );
+
+  await act(async () => {
+    expect(true).toBeTruthy();
+  });
 });
